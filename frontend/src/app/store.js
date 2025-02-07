ea2ba466018ad16ef42fc1eda4d28a256e83f598
raushan1133 <raushankashu@gmail.com>
2025-02-07 14:01:14 +0530

    fixed file issues

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 // import { userSlice } from '@/features/UserSlice'
-import userReducer from '../features/UserSlice'
+import userReducer from '../features/userSlice.js'
 
 export default configureStore({
   reducer: {
